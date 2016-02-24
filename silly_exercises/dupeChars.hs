bce72ba32121b4143682d0f56cd68cba48ef6ae5
Dave McKenna <davemckenna01@gmail.com>
2016-02-24 09:09:54 -0500

    wip hashmap w/ dupechars

--- silly_exercises/dupeChars.hs
+++ silly_exercises/dupeChars.hs
@@ -1,3 +1,7 @@
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+-- O(n^2)
 hasDupes :: [Char] -> Bool
 hasDupes [] = False
 hasDupes (x:xs) = 
@@ -11,5 +15,19 @@ contains1 x [] = False
 contains1 x (y:ys) 
     | x == y    = True
     | otherwise = contains1 x ys
+
+
+-- O(log n) ?
+
+chars :: Map Char Bool
+chars = Map.empty
+
+hasDupes' :: [Char] -> Maybe Bool
+hasDupes' [] = Just False
+hasDupes' (x:xs) =
+    if Map.lookup x chars == Nothing
+        then Nothing
+    else
+        Map.insert x True chars;
 
 
