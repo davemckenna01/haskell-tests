35fa809ad447a2964a47dd9102c622af91beba90
Dave McKenna <davemckenna01@gmail.com>
2016-04-15 21:37:17 -0400

    clean up

--- multi_type_var_functors.hs
+++ multi_type_var_functors.hs
@@ -8,12 +8,12 @@ data Either a b = Left a | Right b
 
 -- fmap can ONLY be defined like this:
 instance Functor (Either c) where   --STYLE 1
-    fmap f (Right x) = Right (f x)  
+    fmap f (Right y) = Right (f y)  
     fmap f (Left x) = Left x
 
 -- we can't define it as:
 
-instance Functor (Either c) where   --STYLE 2
+instance Functor (Either c) where   --STYLE 2 (Won't compile!)
     fmap f (Right y) = Right (y)  
     fmap f (Left x) = Left (f x)
 
@@ -23,4 +23,4 @@ fmap :: (a -> b) -> (Either c) a -> (Either c) b
 -- So we know f takes a to b. If we define fmap as STYLE 2, we're saying
 -- whatever type x is with (Left x), that's the type of c in (Either c).
 -- And since we'd be taking x to b, that would mean the return value would be
--- (Either b) b, which it can't be b/c we defined it as (Either c)!
+-- (Either b) b, which it can't be b/c we defined it as (Either c) b!
