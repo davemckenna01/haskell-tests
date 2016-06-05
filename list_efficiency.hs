b5b8b190b58827c49bcd3517db32108425830d61
Dave McKenna <davemckenna01@gmail.com>
2016-06-05 00:01:00 -0400

    start list efficiency

---
+++ list_efficiency.hs
@@ -0,0 +1,28 @@
+-- be careful when appending things to a list. There's an ineffecient way to
+-- do it.
+
+-- the way ++ is defined, it requres the left arg to be a list, so if it sees
+-- (a ++ b) as the left arg, it goes ahead and constructs (a ++ b) into a list
+-- before appending the new list items. This is inefficient. Better to make sure
+-- the left arg is already a list.
+
+(++) []     ys = ys
+(++) (x:xs) ys = x : xs ++ ys
+
+
+a ++ (b ++ c) -- 4 steps
+
+["a"] ++ (["b"] ++ ["c"])
+"a" : [] ++ (["b"] ++ ["c"])
+"a" : (["b"] ++ ["c"])
+"a" : ("b" : ["c"])
+
+
+(a ++ b) ++ c -- 6 steps (and think exponentially as list grows)
+
+(["a"] ++ ["b"]) ++ ["c"]
+("a" : [] ++ ["b"]) ++ ["c"]
+("a" : ["b"]) ++ ["c"]
+"a" : ["b"] ++ ["c"]
+"a" : "b" : [] ++ ["c"]
+"a" : "b" : ["c"]
