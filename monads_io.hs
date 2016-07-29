4fb9562e9e0d61186299d936ae87a62e19b214cb
Dave McKenna <davemckenna01@gmail.com>
2016-07-29 00:33:41 -0400

    modify with my interpretation of how it works

--- monads_io.hs
+++ monads_io.hs
@@ -26,7 +26,7 @@ getChar =  hGetChar stdin
 --file handle (this is not right usage of term) gives it a "z" (or whatever)
 
 --simplified getChar for my puny mind:
-getChar = IO \world -> (world, "z")
+getChar = IO \world -> (world, Char)
 
 -- Definition of the IO monad's >>= :
 
@@ -49,9 +49,11 @@ main :: IO ()
 main = getChar >>= (\ch1 -> getChar >>= (\ch2 -> return ()))
      = getChar bindIO (\ch1 -> getChar bindIO (\ch2 -> return ()))
      = bindIO getChar (\ch1 -> bindIO getChar (\ch2 -> return ()))
-     = bindIO (IO \world -> (world, "y")) (\ch1 -> bindIO (IO \world -> (world, "z")) ( \ch2 -> IO \world -> (world, ()) ) )
-     IO (\ s -> case (\world -> (world, "y")) s of (new_s, a) -> unIO (       (\ch1 -> bindIO (IO \world -> (world, "z")) ( \ch2 -> IO \world -> (world, ()) ) )             a) new_s)
-     IO (\ s -> case (\world -> (world, "y")) s of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s)
+     -- || is my silly way of representing the "impure primitive" outside world
+     -- interaction that getChar performs
+     = bindIO (IO \world -> (world, ||)) (\ch1 -> bindIO (IO \world -> (world, ||)) ( \ch2 -> IO \world -> (world, ()) ) )
+     IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (       (\ch1 -> bindIO (IO \world -> (world, ||)) ( \ch2 -> IO \world -> (world, ()) ) )             a) new_s)
+     IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s)
 
      -- Fully expanded now. Time to reduce.
 
@@ -59,22 +61,34 @@ main = getChar >>= (\ch1 -> getChar >>= (\ch2 -> return ()))
      --discarding the IO constructor part at the beginning)
      -- TODO: find the code where C-land does this
 
-     (\ s -> case (\world -> (world, "y")) s of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s) 123
-     case (\world -> (world, "y")) 123 of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s
-     case (123, "y") of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s
-     unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )      "y") 123
-     unIO (       IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s)  ) 123
+     (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s) 123
+     case (\world -> (world, ||)) 123 of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s
+     -- DING DING! 123  became 456, as is the way of the magic (State# RealWorld) value
+     -- and || sucked up a "y" from the outside world
+     case (456, "y") of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s
+     unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )      "y") 456
+     unIO (       IO (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s)  ) 456
      -- note "y" vanishes b/c we don't use it
-     (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s)   123
-     case (\world -> (world, "z")) 123 of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s
-     case (123, "z") of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s
-     unIO (( \ch2 -> IO \world -> (world, ()) ) "z") 123
+     (\ s -> case (\world -> (world, ||)) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s)   456
+     case (\world -> (world, ||)) 456 of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s
+     -- DING DING! 456  became 789 and || sucked up a "z" from the outside world
+     case (789, "z") of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s
+     unIO (( \ch2 -> IO \world -> (world, ()) ) "z") 789
      -- note "z" vanishes b/c we don't use it
-     unIO ( IO \world -> (world, ()) ) 123
-     \world -> (world, ()) 123
-     (123, ())
+     unIO ( IO \world -> (world, ()) ) 789
+     \world -> (world, ()) 789
+     (789, ())
+     -- main return s this lonesome useless tuple, now that the program has
+     -- finished running. But wait, this doesn't match up with IO (), which
+     -- is the type of main. Is it because I discarded IO once main kicked off
+     -- with a world int? Even then, the return would be IO (780, ()) which
+     -- seems off. Oh well.
 
-     -- note that 123 isn't really 123. Read more below:
+     -- note that I'm basically guessing at the usage of || and 123 etc,
+     -- and also guessing at the order of evaluation. Look in to the reality of
+     -- these things.  
+
+     -- note that 123 isn't really 123 or 456 etc. Read more below:
 {-
 the State# RealWorld is simply used to preserve statement order:
 "GHC uses impure primitives to implement these IO operations (getChar etc); the State# RealWorld
