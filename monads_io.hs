315de371b5a354089a8024d37abfb32b15cedbfe
Dave McKenna <davemckenna01@gmail.com>
2016-07-29 00:08:53 -0400

    day in life of monadic IO

--- monads_io.hs
+++ monads_io.hs
@@ -1,6 +1,8 @@
 --IO a really means the following:
 newtype IO a = IO (State# RealWorld -> (# State# RealWorld, a #))
 
+-- so when getChar says it's an IO a, it's really a IO (State# RealWorld -> (# State# RealWorld, a #))
+
 {-
 State# is an "unboxed type" which is an optimization that makes it refer
 to a primitive value (think C values like int and float) rather than a thunk,
@@ -17,54 +19,18 @@ newtype IO a = IO (Int -> (Int, a))
 getChar :: IO Char
 getChar =  hGetChar stdin
 
-what is "stdin"?
-
+--hGetChar is a frightening rabbit-hole that you will never return from. Find
+--it here:
 --https://github.com/ghc/ghc/blob/master/libraries/base/GHC/IO/Handle/Text.hs
-hGetChar :: Handle -> IO Char
-hGetChar handle =
-  wantReadableHandle_ "hGetChar" handle $ \handle_@Handle__{..} -> do
+--suffice it to say that getChar blocks the thread and waits till the stdin
+--file handle (this is not right usage of term) gives it a "z" (or whatever)
 
-  -- buffering mode makes no difference: we just read whatever is available
-  -- from the device (blocking only if there is nothing available), and then
-  -- return the first character.
-  -- See [note Buffered Reading] in GHC.IO.Handle.Types
-  buf0 <- readIORef haCharBuffer
+--simplified getChar for my puny mind:
+getChar = IO \world -> (world, "z")
 
-  buf1 <- if isEmptyBuffer buf0
-             then readTextDevice handle_ buf0
-             else return buf0
+-- Definition of the IO monad's >>= :
 
-  (c1,i) <- readCharBuf (bufRaw buf1) (bufL buf1)
-  let buf2 = bufferAdjustL i buf1
-
-  if haInputNL == CRLF && c1 == '\r'
-     then do
-            mbuf3 <- if isEmptyBuffer buf2
-                      then maybeFillReadBuffer handle_ buf2
-                      else return (Just buf2)
-
-            case mbuf3 of
-               -- EOF, so just return the '\r' we have
-               Nothing -> do
-                  writeIORef haCharBuffer buf2
-                  return '\r'
-               Just buf3 -> do
-                  (c2,i2) <- readCharBuf (bufRaw buf2) (bufL buf2)
-                  if c2 == '\n'
-                     then do
-                       writeIORef haCharBuffer (bufferAdjustL i2 buf3)
-                       return '\n'
-                     else do
-                       -- not a \r\n sequence, so just return the \r
-                       writeIORef haCharBuffer buf3
-                       return '\r'
-     else do
-            writeIORef haCharBuffer buf2
-            return c1
-
-what is handle_@Handle__{..}, and what is ANY OF IT?!
-
-
+>>= = bindIO
 
 bindIO :: IO a -> (a -> IO b) -> IO b
 bindIO (IO m) k = IO (\ s -> case m s of (# new_s, a #) -> unIO (k a) new_s)
@@ -78,30 +44,53 @@ unIO :: IO a -> (Int -> (Int, a))
 unIO (IO a) = a
 
 
+-- a program that reads 2 chars and then exits
 main :: IO ()
-main = getChar >>= (\ch1 -> return ())
-     = getChar bindIO (\ch1 -> return ())
-     = bindIO getChar (\ch1 -> return ())
+main = getChar >>= (\ch1 -> getChar >>= (\ch2 -> return ()))
+     = getChar bindIO (\ch1 -> getChar bindIO (\ch2 -> return ()))
+     = bindIO getChar (\ch1 -> bindIO getChar (\ch2 -> return ()))
+     = bindIO (IO \world -> (world, "y")) (\ch1 -> bindIO (IO \world -> (world, "z")) ( \ch2 -> IO \world -> (world, ()) ) )
+     IO (\ s -> case (\world -> (world, "y")) s of (new_s, a) -> unIO (       (\ch1 -> bindIO (IO \world -> (world, "z")) ( \ch2 -> IO \world -> (world, ()) ) )             a) new_s)
+     IO (\ s -> case (\world -> (world, "y")) s of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s)
 
+     -- Fully expanded now. Time to reduce.
 
+     --now imagine C-land giving main a kick-off world value of 123 (after
+     --discarding the IO constructor part at the beginning)
+     -- TODO: find the code where C-land does this
 
+     (\ s -> case (\world -> (world, "y")) s of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s) 123
+     case (\world -> (world, "y")) 123 of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s
+     case (123, "y") of (new_s, a) -> unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )             a) new_s
+     unIO (       (\ch1 -> IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s) )      "y") 123
+     unIO (       IO (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s)  ) 123
+     -- note "y" vanishes b/c we don't use it
+     (\ s -> case (\world -> (world, "z")) s of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s)   123
+     case (\world -> (world, "z")) 123 of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s
+     case (123, "z") of (new_s, a) -> unIO (( \ch2 -> IO \world -> (world, ()) ) a) new_s
+     unIO (( \ch2 -> IO \world -> (world, ()) ) "z") 123
+     -- note "z" vanishes b/c we don't use it
+     unIO ( IO \world -> (world, ()) ) 123
+     \world -> (world, ()) 123
+     (123, ())
 
-
-
-http://stackoverflow.com/questions/9244538/what-are-the-definitions-for-and-return-for-the-io-monad
-https://hackage.haskell.org/package/base-4.9.0.0/docs/src/GHC.IO.html
-- "The IO monad is just an instance of the State-Transformer monad"
-https://hackage.haskell.org/package/base-4.9.0.0/docs/src/GHC.ST.html
-
-the State# is simply used to preserve statement order:
-"GHC uses impure primitives to implement these IO operations; the State# RealWorld
+     -- note that 123 isn't really 123. Read more below:
+{-
+the State# RealWorld is simply used to preserve statement order:
+"GHC uses impure primitives to implement these IO operations (getChar etc); the State# RealWorld
 "values" are only to stop the compiler reordering statements by introducing data
 dependencies from one statement to the next."
-... it's not actually the entire state of the world!
-The IO monad is not a state monad. The state monad looks very similar to the type GHC uses for IO. Thus the confusion.
-Notice the reversed order of values in state vs io return tuple.
+... it's not actually the entire state of the world! It's just some arbitrary number.
 
+Note also:
+The IO monad is not a state monad, despite popular belief. The state monad looks very similar to the type GHC uses for IO. Thus the confusion.
+Notice the reversed order of values in state vs io return tuple.
 (from http://stackoverflow.com/questions/9244538/what-are-the-definitions-for-and-return-for-the-io-monad)
+See for yourself:
+https://hackage.haskell.org/package/base-4.9.0.0/docs/src/GHC.IO.html
+https://hackage.haskell.org/package/base-4.9.0.0/docs/src/GHC.ST.html
+-}
+
 
 
 
